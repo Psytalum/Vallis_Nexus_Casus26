3a65f066f05a820ccf5a20716ce58282a7740adf
Nino Constansia <2306292constansia@zuyd.nl>
2026-06-23 11:25:59 +0200

    Basic solution

---
+++ Casus_Vallis_Nexus/Program.cs
@@ -0,0 +1,23 @@
+﻿using Vallis_Nexus_Casus26;
+
+namespace Casus_Vallis_Nexus
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DAL Vallis_dal = new DAL();
+
+            try
+            {
+                Vallis_dal.InsertRating(8.7m);
+                Console.WriteLine("Verbinding gelukt! Data ingevoegd.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fout: " + ex.Message);
+            }
+
+        }
+    }
+}
