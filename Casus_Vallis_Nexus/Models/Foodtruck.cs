284d846b4713c6fd0bd9bf74f5e5b9d3cda6551e
khlifahmad-codes <khlif.ahmad97@gmail.com>
2026-07-02 12:52:54 +0200

    Add files via upload

---
+++ Casus_Vallis_Nexus/Models/Foodtruck.cs
@@ -0,0 +1,17 @@
+﻿using Casus_Vallis_Nexus.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VallisNexusFestival
+{
+    public class Foodtruck : Locatie
+    {
+        public Foodtruck(int id, string naam, string info, string status)
+            : base(id, naam, info, status)
+        {
+        }
+    }
+}
---
+++ Casus_Vallis_Nexus/Models/Plattegrond.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+
+namespace Casus_Vallis_Nexus.Models
+{
+    public class Plattegrond
+    {
+        public int PlattegrondId { get; set; }
+        public string TerreinNaam { get; set; }
+        public List<Locatie> Locaties { get; set; }
+
+        public Plattegrond(int plattegrondId, string terreinNaam)
+        {
+            PlattegrondId = plattegrondId;
+            TerreinNaam = terreinNaam;
+            Locaties = new List<Locatie>();
+        }
+
+        public void VoegLocatieToe(Locatie locatie)
+        {
+            Locaties.Add(locatie);
+        }
+
+        public void ToonPlattegrond()
+        {
+            Console.WriteLine($"=== Plattegrond van {TerreinNaam} ===");
+
+            foreach (Locatie locatie in Locaties)
+            {
+                Console.WriteLine($"{locatie.LocatieId} - {locatie.Naam} ({locatie.GetType().Name})");
+            }
+        }
+
+        public Locatie ZoekLocatieOpId(int id)
+        {
+            foreach (Locatie locatie in Locaties)
+            {
+                if (locatie.LocatieId == id)
+                {
+                    return locatie;
+                }
+            }
+
+            return null;
+        }
+    }
+}
